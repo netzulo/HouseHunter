d3ad0c80a0a8c25fb7b566c7a396f007eab30108
josue.garcia <josue.garcia@gigas.com>
2018-10-30 15:14:44 +0100

    [hh] now config can have more than 1 page search

--- idealista.py
+++ idealista.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*- 
 
 
+import time
 import datetime
-import email.message
+import email.message as EmailMessage
 import smtplib 
 
 from qacode.core.bots.bot_base import BotBase
 from qacode.core.webs.pages.page_base import PageBase
-from qacode.core.webs.controls.control_base import ControlBase
 from qautils.files import settings
 
 
@@ -62,83 +62,90 @@ PASS = IDEALISTA["data"]["pass"]
 EMAIL_SRC = IDEALISTA["data"]["email_src"]
 EMAIL_DST = IDEALISTA["data"]["email_dst"]
 
+def main():
+    bot = None
+    try:
+        curr_date = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+        results = []
+        # START
+        bot = BotBase(**SETTINGS)
+        for page_config in IDEALISTA["pages"]:
+            page = PageBase(bot, **page_config)
+            # flats found : .i
+            # tems-container .item 
+            flats = page.flat_containers.elements
+            bot.log.info("Found flats : {}".format(len(flats)))
+            for flat_container in flats:
+                # title
+                _title = flat_container.find_element(BY, ".item-info-container>a")
+                title_href = _title.get_attribute("href")
+                title = _title.text
+                # price
+                price = flat_container.find_element(BY, ".item-info-container .item-price.h2-simulated").text
+                # details
+                details = ''
+                detail_elements = flat_container.find_elements(BY, ".item-info-container .item-detail")
+                for detail in detail_elements:
+                    details += detail.text
+                # contact
+                contact = flat_container.find_element(BY, ".item-toolbar-contact a span").text
+                # Parse
+                result = {
+                    "price": price,
+                    "details": details,
+                    "contact_href": contact.replace(" ", ""),
+                    "contact": contact,
+                    "title_href": title_href,
+                    "title": title
+                }
+                results.append(result)
+            # END
+            if len(flats) <= 0:
+                page.error_ddos.reload()
+                if page.error_ddos.element is None:
+                    bot.log.warning("Not going to send an email for 0 flats founds")
+                raise InterruptedError(1, "IDEALISTA think you are performing DDos attack")
+            email_rows_str = ''
+            bot.log.info("EMAIL: building...")
+            for result in results:
+                email_row = EMAIL_TEMPLATE_ROW.format(
+                    result['price'],
+                    result['contact_href'],
+                    result['contact'],
+                    result['details'],
+                    result['title_href'],
+                    result['title']
+                )
+                email_rows_str += (email_row)
+            bot.log.info("EMAIL: adding HTML content...")
+            email_html = EMAIL_TEMPLATE.format(email_rows_str)
+            email = EmailMessage.Message()
+            email ['Subject'] = "IDEALISTA | Flat search - {}".format(curr_date)
+            email['From'] = EMAIL_SRC
+            email['To'] = EMAIL_DST
+            email.add_header('Content-Type','text/html')
+            email.set_payload(email_html)
+            bot.log.info("EMAIL: added HTML content...")
+            bot.log.info("GMAIL: connecting...")
+            server = smtplib.SMTP('smtp.gmail.com', 587)
+            server.ehlo()
+            server.starttls()
+            server.login(USER, PASS)
+            bot.log.info("GMAIL: connected!")
+            bot.log.info("GMAIL: send an email")
+            server.sendmail(email['From'], email['To'], email.as_string().encode())
+            bot.log.info("GMAIL: email sent!")
+            server.close()
+            # END, next iteration
+            bot.log.info("Hardcoded waiting to avoid idealista think it's DDos Error")
+            bot.log.info("Waiting 15 seconds before continue")
+            time.sleep(15)
+    except Exception as err:
+        bot.log.error("ERROR: {}".format(err))
+    finally:
+        bot.log.info("BOT: closing...")
+        bot.close()
+        bot.log.info("BOT: closed!")
 
-bot = None
-
-try:
-    curr_date = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
-    results = []
-    # START
-    bot = BotBase(**SETTINGS)
-    page = PageBase(bot, **IDEALISTA["pages"][0])
-    # flats found : .i
-    # tems-container .item 
-    flats = page.flat_containers.elements
-    bot.log.info("Found flats : {}".format(len(flats)))
-    for flat_container in flats:
-        # title
-        _title = flat_container.find_element(BY, ".item-info-container>a")
-        title_href = _title.get_attribute("href")
-        title = _title.text
-        # price
-        price = flat_container.find_element(BY, ".item-info-container .item-price.h2-simulated").text
-        # details
-        details = ''
-        detail_elements = flat_container.find_elements(BY, ".item-info-container .item-detail")
-        for detail in detail_elements:
-            details += detail.text
-        # contact
-        contact = flat_container.find_element(BY, ".item-toolbar-contact a span").text
-        # Parse
-        result = {
-            "price": price,
-            "details": details,
-            "contact_href": contact.replace(" ", ""),
-            "contact": contact,
-            "title_href": title_href,
-            "title": title
-        }
-        results.append(result)
-    # END
-    if len(flats) <= 0:
-        page.error_ddos.reload()
-        if page.error_ddos.element is None:
-            bot.log.warning("Not going to send an email for 0 flats founds")
-        raise InterruptedError(1, "IDEALISTA think you are performing DDos attack")
-    email_rows_str = ''
-    bot.log.info("EMAIL: building...")
-    for result in results:
-        email_row = EMAIL_TEMPLATE_ROW.format(
-            result['price'],
-            result['contact_href'],
-            result['contact'],
-            result['details'],
-            result['title_href'],
-            result['title']
-        )
-        email_rows_str += (email_row)
-    bot.log.info("EMAIL: adding HTML content...")
-    email_html = EMAIL_TEMPLATE.format(email_rows_str)
-    email = email.message.Message()
-    email ['Subject'] = "IDEALISTA | Flat search - {}".format(curr_date)
-    email['From'] = EMAIL_SRC
-    email['To'] = EMAIL_DST
-    email.add_header('Content-Type','text/html')
-    email.set_payload(email_html)
-    bot.log.info("EMAIL: added HTML content...")
-    bot.log.info("GMAIL: connecting...")
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.ehlo()
-    server.starttls()
-    server.login(USER, PASS)
-    bot.log.info("GMAIL: connected!")
-    bot.log.info("GMAIL: send an email")
-    server.sendmail(email['From'], email['To'], email.as_string().encode())
-    bot.log.info("GMAIL: email sent!")
-    server.close()
-except Exception as err:
-    bot.log.error("ERROR: {}".format(err))
-finally:
-    bot.log.info("BOT: closing...")
-    bot.close()
-    bot.log.info("BOT: closed!")
+# STARTs
+main()
